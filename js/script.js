740b07ef840ea301d4f34f49b2de95a9fadc2dee
KatKusa <kusa2706@mail.ru>
2019-10-18 00:44:29 +0300

    сделано задание
    сделана полностью стилизация всего

---
+++ js/script.js
@@ -0,0 +1,34 @@
+var link = document.querySelector(".user-info");
+var popup = document.querySelector(".modal");
+var close = popup.querySelector(".modal-close");
+var form = popup.querySelector(".userfield-form");
+var username = popup.querySelector("[name=username]");
+var email = popup.querySelector("[name=user-mail]");
+var message = popup.querySelector("[name=user-message]");
+
+
+link.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.add("modal-show");
+  username.focus();
+});
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("modal-show");
+  popup.classList.remove("modal-error");
+});
+form.addEventListener("submit", function (evt) {
+  if (!username.value || !email.value || !message.value) {
+    evt.preventDefault();
+    popup.classList.add("modal-error");
+  }
+});
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    evt.preventDefault();
+    if (popup.classList.contains("modal-show")) {
+      popup.classList.remove("modal-show");
+      popup.classList.remove("modal-error");
+    }
+  }
+});
