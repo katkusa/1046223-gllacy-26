79480e610ab098196ac0f8eb4750a305d23a33c2
KatKusa <kusa2706@mail.ru>
2019-10-22 19:26:05 +0300

    фиииинааал

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 var link = document.querySelector(".user-info");
 var popup = document.querySelector(".modal");
+var area = popup.querySelector(".modal-content");
 var close = popup.querySelector(".modal-close");
 var form = popup.querySelector("[name=userfield-form]");
 var username = popup.querySelector("[name=username]");
@@ -10,9 +11,18 @@ var message = popup.querySelector("[name=user-message]");
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.add("modal-show");
+  area.classList.add("modal-show-animation");
   username.focus();
 });
 close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.remove("modal-show");
+  popup.classList.remove("modal-error");
+});
+popup.addEventListener("click", function (evt) {
+  if (!evt.target.classList.contains('modal')) {
+    return;
+  }
   evt.preventDefault();
   popup.classList.remove("modal-show");
   popup.classList.remove("modal-error");
