0dd535fb403e7b073e1fb62bc0d7ff3104d13993
KatKusa <kusa2706@mail.ru>
2019-10-24 14:55:03 +0300

    Правки после проверки

--- js/script.js
+++ js/script.js
@@ -30,9 +30,9 @@ popup.addEventListener("click", function (evt) {
 form.addEventListener("submit", function (evt) {
   if (!username.value || !email.value || !message.value) {
     evt.preventDefault();
-    popup.classList.remove("modal-error");
+    area.classList.remove("modal-error");
     popup.offsetWidth = popup.offsetWidth;
-    popup.classList.add("modal-error");
+    area.classList.add("modal-error");
   }
 });
 window.addEventListener("keydown", function (evt) {
